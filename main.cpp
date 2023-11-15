f9043c57b9129e393be4c7492d4b8ffb5af5609b
Dmitry Pimenov <dimapim99@gmail.com>
2023-11-16 00:42:22 +0300

    Processing program arguments, reading a matrix from a file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <vector>
+#include "readMatrix.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc <= 1 || argc >= 4) {
+        std::cerr << "Invalid number of arguments." << std::endl;
+        exit(1);
+    }
+
+    size_t matrixSize;
+    matrixSize = strtoul(argv[1], nullptr, 10);
+    std::vector<std::vector<double>> matrix(matrixSize, std::vector<double> (matrixSize));
+    if (matrixSize == 0) {
+        std::cerr << "The first argument is not a positive integer." << std::endl;
+        exit(1);
+    }
+
+    if (argc == 2) {
+//        TODO create matrix by formula
+    }
+    else {
+        readMatrix(argv[2], matrixSize, matrix);
+    }
+
+//    for (auto& it : matrix) {
+//        for (double i : it)
+//            std::cout << i << ' ';
+//        std::cout << std::endl;
+//    }
+
     return 0;
 }
